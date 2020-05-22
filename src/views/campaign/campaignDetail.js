28a2b46bc09dc5aee8a5faacadda7fc9eb904904
cybertriton <li.zhangc@outlook.com>
2020-05-22 20:29:36 +0800

    Update campaignDetail.js

--- src/views/campaign/campaignDetail.js
+++ src/views/campaign/campaignDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Card, Descriptions, Input, Drawer, Table, Collapse } from "antd";
 import CampaignMerchant from "./campaignMerchant";
 import "./index.less";
@@ -49,7 +49,7 @@ class CampaignDetail extends Component {
   descriptionItem = () => {
     const setTitle = [
       {
-        title: "优惠券名称",
+        title: "名称",
         value: "name",
       },
       {
@@ -74,14 +74,14 @@ class CampaignDetail extends Component {
         title: "发行方式",
         value: "multiple", //true 一码一券
         type: "boolean",
-        bolValue: ["一码一券", "-"],
+        bolValue: ["一码一券", "通用码"],
       },
       {
         title: "发行数量",
         value: "totalSupply",
       },
       {
-        title: "是否自动增发",
+        title: "是否允许增发",
         value: "autoUpdate",
         type: "boolean",
         bolValue: ["是", "否"],
@@ -106,18 +106,81 @@ class CampaignDetail extends Component {
       </div>
     );
   };
+  
+  _renderConfigPanel(voucherConfig) {
+    let discountContent = null;
+    if (voucherConfig && voucherConfig.discount && voucherConfig.discount.type === "PERCENT") {
+      discountContent = (
+        <Fragment>
+        <Descriptions.Item label="类型">折扣券</Descriptions.Item>
+        <Descriptions.Item label="折扣">
+          {voucherConfig.discount.valueOff}% 
+        </Descriptions.Item>                
+        <Descriptions.Item label="最高优惠">
+          {voucherConfig.discount.amountLimit ? voucherConfig.discount.amountLimit + "元" : "无限制"}
+        </Descriptions.Item>
+        </Fragment>
+      )
+    }
+
+    if (voucherConfig && voucherConfig.discount && voucherConfig.discount.type === "AMOUNT") {
+      discountContent = (
+        <Fragment>
+        <Descriptions.Item label="类型">
+        代金券
+        </Descriptions.Item>
+        <Descriptions.Item label="金额">
+            {voucherConfig.discount.valueOff / 100}元 
+        </Descriptions.Item> 
+        </Fragment>
+      )
+    }
+
+    if (voucherConfig) {    
+      return (
+        <Descriptions size="small" bordered column={1}>
+        <Descriptions.Item label="名称">
+          {voucherConfig.name}
+        </Descriptions.Item>
+        {discountContent}
+        {voucherConfig.daysAfterDist ? (
+          <Descriptions.Item label="有效时间">
+              领取/发放后 {voucherConfig.daysAfterDist} 天内有效
+          </Descriptions.Item>
+          ) : (
+            <Descriptions.Item label="有效时间">
+              {voucherConfig.effective}至{voucherConfig.expiry}
+            </Descriptions.Item>
+          )}
+          <Descriptions.Item label="发行方式">
+            {voucherConfig.multiple ? "一码一券" : "-"}
+          </Descriptions.Item>
+          <Descriptions.Item label="发行数量">
+            {voucherConfig.totalSupply ? voucherConfig.totalSupply : "-"}
+          </Descriptions.Item>
+          <Descriptions.Item label="发行方式">
+            {voucherConfig.autoUpdate ? "是" : "否"}
+          </Descriptions.Item>
+        </Descriptions>
+      )
+    } else {
+      return "";
+    }
+  };
+
   renderContent = () => {
     const { curInfo, visible, listSize } = this.state;
     const { voucherConfig } = this.state.curInfo;
     return (
       <Drawer
         className="camDetail"
-        width={650}
+        width={500}
         title="活动详情"
         onClose={this.onClose}
         visible={visible}
       >
-        <Card bordered={false} className="detail">
+        <Collapse defaultActiveKey={['1']}>
+          <Panel header="基本信息" key="1">
           <Descriptions
             size="small"
             bordered
@@ -145,81 +208,30 @@ class CampaignDetail extends Component {
               {curInfo.description}
             </Descriptions.Item>
           </Descriptions>
-        </Card>
-        {/* 优惠券名称 类型 代金券 折扣券 折扣比例 % 最高优惠金额 元 有效期
-        固定有效时间 2020-05-22 → 2020-05-23 相对有效时间 发放/领取后 天有效
-        发行方式 一码一券 发行数量 1 是否自动增发 */}
-        {curInfo.voucherConfig ? (
-          <Card bordered={false} className="detail">
-            <Descriptions size="small" bordered column={1}>
-              <Descriptions.Item label="优惠券名称">
-                {voucherConfig.name ? voucherConfig.name : "-"}
-              </Descriptions.Item>
-              <Descriptions.Item label="类型">
-                {voucherConfig.discount.type === "AMOUNT" ? "代金券" : "折扣券"}
-              </Descriptions.Item>
-              {voucherConfig.discount.type === "PERCENT" ? (
-                <Descriptions.Item label="折扣比例">
-                  {voucherConfig.discount && voucherConfig.discount.valueOff
-                    ? voucherConfig.discount.valueOff
-                    : "-"}
-                </Descriptions.Item>
-              ) : (
-                <Descriptions.Item label="金额">
-                  {voucherConfig.discount && voucherConfig.discount.valueOff
-                    ? parseFloat(voucherConfig.discount.valueOff) / 100
-                    : "-"}
-                </Descriptions.Item>
-              )}
-              {voucherConfig.discount.type === "PERCENT" ? (
-                <Descriptions.Item label="最高优惠金额">
-                  {voucherConfig.discount && voucherConfig.discount.amountLimit
-                    ? voucherConfig.discount.amountLimit
-                    : "-"}
-                </Descriptions.Item>
-              ) : null}
-              {voucherConfig.daysAfterDist ? (
-                <Descriptions.Item label="相对有效时间">
-                  {voucherConfig.daysAfterDist}天
-                </Descriptions.Item>
-              ) : (
-                <Descriptions.Item label="固定有效时间">
-                  {voucherConfig.effective}至{voucherConfig.expiry}
-                </Descriptions.Item>
-              )}
-
-              <Descriptions.Item label="发行方式">
-                {voucherConfig.multiple ? "一码一券" : "-"}
-              </Descriptions.Item>
-              <Descriptions.Item label="发行数量">
-                {voucherConfig.totalSupply ? voucherConfig.totalSupply : "-"}
-              </Descriptions.Item>
-              <Descriptions.Item label="发行方式">
-                {voucherConfig.autoUpdate ? "是" : "否"}
-              </Descriptions.Item>
-            </Descriptions>
-          </Card>
-        ) : null}
-        <Card bordered={false} className="detail">
-          <Collapse accordion>
-            <Panel header="参与商户" key="1">
-              <Table
-                bordered
-                size="small"
-                className="tableFont"
-                columns={this.columns}
-                dataSource={curInfo.parties}
-                pagination={{
-                  pageSize: listSize,
-                  total:
-                    curInfo && curInfo.parties && curInfo.parties.length !== 0
-                      ? curInfo.parties.length
-                      : 0,
-                }}
-              />
-            </Panel>
-          </Collapse>
-        </Card>
+          </Panel>
+          <Panel header="详细设置" key="2">
+          {/* 优惠券名称 类型 代金券 折扣券 折扣比例 % 最高优惠金额 元 有效期
+          固定有效时间 2020-05-22 → 2020-05-23 相对有效时间 发放/领取后 天有效
+          发行方式 一码一券 发行数量 1 是否自动增发 */}
+            {this._renderConfigPanel(voucherConfig)}
+          </Panel>
+          <Panel header="参与商户" key="3">
+            <Table
+              bordered
+              size="small"
+              className="tableFont"
+              columns={this.columns}
+              dataSource={curInfo.parties}
+              pagination={{
+                pageSize: listSize,
+                total:
+                  curInfo && curInfo.parties && curInfo.parties.length !== 0
+                    ? curInfo.parties.length
+                    : 0,
+              }}
+            />
+          </Panel>
+        </Collapse>
       </Drawer>
     );
   };
