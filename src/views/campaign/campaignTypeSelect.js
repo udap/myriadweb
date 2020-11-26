4ce2f853850d48fbb939a83205dd8d8835c10cde
leyliu <leyliu@leyliu.com>
2020-11-26 15:37:37 +0800

    :bookmark: Open GIFT type annotation

--- src/views/campaign/campaignTypeSelect.js
+++ src/views/campaign/campaignTypeSelect.js
@@ -14,14 +14,14 @@ const CampaignTypes = [
     desc:
       "电子优惠券是企业利用App、短信、社交媒体等多种渠道进行数字化营销的便利手段。每一张优惠券都有一个唯一的、随机产生的编码，可以有效防止欺诈。",
   },
-  // {
-  //   name: "礼品换购活动",
-  //   icon: "GiftOutlined",
-  //   type: "VOUCHER",
-  //   subType: "GIFT", //VOUCHER, PROMOTION
-  //   desc:
-  //     "礼品换购活动允许企业利用性价比高的热门礼品实现低成本营销，在不降低利润的前提下为用户营造超值的优惠力度，完成引流、截流、回流、现金流和营业额的突破。",
-  // },
+  {
+    name: "礼品换购活动",
+    icon: "GiftOutlined",
+    type: "VOUCHER",
+    subType: "GIFT", //VOUCHER, PROMOTION
+    desc:
+      "礼品换购活动允许企业利用性价比高的热门礼品实现低成本营销，在不降低利润的前提下为用户营造超值的优惠力度，完成引流、截流、回流、现金流和营业额的突破。",
+  },
 ];
 
 const CampaignTypeSelect = (props) => {
